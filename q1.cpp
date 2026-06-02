6abb1cc4ce5dd964ee86664a3103febda63bcbb4
Abhishekverma4575 <abhishekkumarverma4565@gmail.com>
2026-06-02 21:17:49 +0530

    Add files via upload

--- q1.cpp
+++ q1.cpp
@@ -1,18 +1,20 @@
-// sum of first N natural number 
+   // summ of digits of number
 
- #include <iostream>
- using namespace std ;
-  
-int main () {
-    cout<< " enter the number upto to sum" ;
+   #include<iostream>
+   using namespace std ;
+
+   int main (){
+    cout<< " enter the number  " ;
     int n;
-    cin>> n;
-    int sum =0 ;
-    int i=1;
-    while( i<=n){
-        sum=sum +i ;
-        i++;
+    cin>> n ;
+    int sum = 0 ;
+    while(n>0){
+        int c ;
+        c  = n % 10 ;
+        sum = sum +c ;
+        n = n/10 ;
     }
-    cout<< " sum is " << sum ;
-    return 0;
-}
+    cout<< sum ;
+    return 0 ;
+     
+   }
